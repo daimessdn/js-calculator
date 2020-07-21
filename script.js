e05214e933f06e7be21bc542edd248fa2e154ee6
daimessdn <daimessdn@gmail.com>
2020-07-21 22:15:46 +0700

    added input number method

--- script.js
+++ script.js
@@ -3,18 +3,28 @@ const numbers = document.querySelectorAll(".number");
 // console.log(numbers);
 
 numbers.forEach((number) => {
-	// console.log(number);
+  // console.log(number);
 
-	number.addEventListener("click", (event) => {
-		// console.log("number is pressed");
+  number.addEventListener("click", (event) => {
+    // console.log("number is pressed");
 
-		// console.log(event.target.value);
-		updateScreen(event.target.value);
-	})
+    // console.log(event.target.value);
+    inputNumber(event.target.value);
+    updateScreen(currentNumber);
+  })
 })
 
 const calculatorScreen = document.querySelector(".calculator-screen");
 
 const updateScreen = (number) => {
-	calculatorScreen.value = number;
+  calculatorScreen.value = number;
+}
+
+//  init'd calculation number
+let prevNumber = "";
+let calculationOperator - "";
+let currentNumber;
+
+const inputNumber = (number) => {
+  currentNumber = number; 
 }
