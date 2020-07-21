08dc3716ea5b4ea1bbac6150c73d68b0c7caaf54
daimessdn <daimessdn@gmail.com>
2020-07-21 22:38:14 +0700

    added 2nd listener method for the operator

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 const numbers = document.querySelectorAll(".number");
 // console.log(numbers);
 
+//  init'd calculation number
+let prevNumber = "";
+let calculationOperator = "";
+let currentNumber = "0";
+
+const inputNumber = (number) => {
+  if (currentNumber === "0") {
+    currentNumber = number;
+  }
+  else {
+    currentNumber += number;
+  }
+};
+
 numbers.forEach((number) => {
   // console.log(number);
 
@@ -18,13 +32,13 @@ const calculatorScreen = document.querySelector(".calculator-screen");
 
 const updateScreen = (number) => {
   calculatorScreen.value = number;
-}
+};
 
-//  init'd calculation number
-let prevNumber = "";
-let calculationOperator - "";
-let currentNumber;
+// let's continue to the operator
+const operators = document.querySelectorAll(".operator");
 
-const inputNumber = (number) => {
-  currentNumber = number; 
-}
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    console.log(event.target.value);
+  })
+});
